a2eda76ebee721782d1be0dd629faf167c639eee
Muhammed Basil <107062974+b4si@users.noreply.github.com>
2022-11-29 10:20:31 +0530

    bug fix

--- lib/screens/transaction_screen/transaction_screen.dart
+++ lib/screens/transaction_screen/transaction_screen.dart
@@ -247,103 +247,113 @@ class TransactionScreenState extends State<TransactionScreen> {
                   ),
                 ),
                 Expanded(
-                  child: ValueListenableBuilder(
-                    valueListenable:
-                        TransactionDB.instance.transactionListNotifier,
-                    builder: (BuildContext context,
-                        List<TransactionModel> newList, _) {
-                      return ListView.separated(
-                        itemBuilder: ((context, index) {
-                          final transactionList = foundTransactions[index];
-                          return Slidable(
-                            key: Key(transactionList.id!),
-                            startActionPane: ActionPane(
-                                motion: const StretchMotion(),
-                                children: [
-                                  SlidableAction(
-                                    onPressed: (ctx) {
-                                      setState(() {
-                                        TransactionDB.instance
-                                            .deleteTransaction(
-                                                transactionList.id!);
-                                      });
-                                    },
-                                    backgroundColor: Colors.red,
-                                    icon: Icons.delete,
-                                    label: "Delete",
-                                  ),
-                                ]),
-                            child: ListTile(
-                              onTap: () {
-                                showDialog(
-                                  context: context,
-                                  builder: ((context) {
-                                    return SimpleDialog(
-                                      contentPadding: const EdgeInsets.all(18),
-                                      title: const Text('Details'),
+                  child: foundTransactions.isEmpty
+                      ? Center(
+                          child: Text(
+                            'Transaction Not found',
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                        )
+                      : ValueListenableBuilder(
+                          valueListenable:
+                              TransactionDB.instance.transactionListNotifier,
+                          builder: (BuildContext context,
+                              List<TransactionModel> newList, _) {
+                            return ListView.separated(
+                              itemBuilder: ((context, index) {
+                                final transactionList =
+                                    foundTransactions[index];
+                                return Slidable(
+                                  key: Key(transactionList.id!),
+                                  startActionPane: ActionPane(
+                                      motion: const StretchMotion(),
                                       children: [
-                                        Text(
-                                          'Category : ${transactionList.category.name}',
+                                        SlidableAction(
+                                          onPressed: (ctx) {
+                                            setState(() {
+                                              TransactionDB.instance
+                                                  .deleteTransaction(
+                                                      transactionList.id!);
+                                            });
+                                          },
+                                          backgroundColor: Colors.red,
+                                          icon: Icons.delete,
+                                          label: "Delete",
                                         ),
-                                        const SizedBox(
-                                          height: 10,
-                                        ),
-                                        Text(
-                                          'Amount   : ${transactionList.amount}',
-                                        ),
-                                        const SizedBox(
-                                          height: 10,
-                                        ),
-                                        Text(
-                                          'Date         : ${parseDate(
-                                            transactionList.date,
-                                          )}',
-                                        ),
-                                        const SizedBox(
-                                          height: 10,
-                                        ),
-                                        Text(
-                                          'Notes       : ${transactionList.notes}',
-                                        ),
-                                      ],
-                                    );
-                                  }),
+                                      ]),
+                                  child: ListTile(
+                                    onTap: () {
+                                      showDialog(
+                                        context: context,
+                                        builder: ((context) {
+                                          return SimpleDialog(
+                                            contentPadding:
+                                                const EdgeInsets.all(18),
+                                            title: const Text('Details'),
+                                            children: [
+                                              Text(
+                                                'Category : ${transactionList.category.name}',
+                                              ),
+                                              const SizedBox(
+                                                height: 10,
+                                              ),
+                                              Text(
+                                                'Amount   : ${transactionList.amount}',
+                                              ),
+                                              const SizedBox(
+                                                height: 10,
+                                              ),
+                                              Text(
+                                                'Date         : ${parseDate(
+                                                  transactionList.date,
+                                                )}',
+                                              ),
+                                              const SizedBox(
+                                                height: 10,
+                                              ),
+                                              Text(
+                                                'Notes       : ${transactionList.notes}',
+                                              ),
+                                            ],
+                                          );
+                                        }),
+                                      );
+                                    },
+                                    title: Text(transactionList.category.name),
+                                    subtitle: Text(
+                                      parseDate(transactionList.date),
+                                    ),
+                                    trailing: transactionList.type ==
+                                            CategoryType.income
+                                        ? Text(
+                                            '₹${transactionList.amount.toString()}',
+                                            style: const TextStyle(
+                                              color: Colors.green,
+                                            ),
+                                          )
+                                        : Text(
+                                            '₹${transactionList.amount.toString()}',
+                                            style: const TextStyle(
+                                              color: Colors.red,
+                                            ),
+                                          ),
+                                  ),
+                                );
+                              }),
+                              separatorBuilder: (context, index) {
+                                return const Padding(
+                                  padding: EdgeInsets.only(right: 15, left: 15),
+                                  child: Divider(
+                                    thickness: 1,
+                                    color: Colors.grey,
+                                  ),
                                 );
                               },
-                              title: Text(transactionList.category.name),
-                              subtitle: Text(
-                                parseDate(transactionList.date),
-                              ),
-                              trailing:
-                                  transactionList.type == CategoryType.income
-                                      ? Text(
-                                          '₹${transactionList.amount.toString()}',
-                                          style: const TextStyle(
-                                            color: Colors.green,
-                                          ),
-                                        )
-                                      : Text(
-                                          '₹${transactionList.amount.toString()}',
-                                          style: const TextStyle(
-                                            color: Colors.red,
-                                          ),
-                                        ),
-                            ),
-                          );
-                        }),
-                        separatorBuilder: (context, index) {
-                          return const Padding(
-                            padding: EdgeInsets.only(right: 15, left: 15),
-                            child: Divider(
-                              thickness: 1,
-                              color: Colors.grey,
-                            ),
-                          );
-                        },
-                        itemCount: foundTransactions.length,
-                      );
-                    },
-                  ),
+                              itemCount: foundTransactions.length,
+                            );
+                          },
+                        ),
                 ),
               ],
             ),
